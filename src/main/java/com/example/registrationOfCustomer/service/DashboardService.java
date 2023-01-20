aa066f26453ce7cc3960e9123f9b45c5c50ae17d
indrarajpant <indrarajpant@gmail.com>
2023-01-19 18:43:52 -0600

    Added @Service to DashboardService

--- src/main/java/com/example/registrationOfCustomer/service/DashboardService.java
+++ src/main/java/com/example/registrationOfCustomer/service/DashboardService.java
@@ -2,7 +2,9 @@ package com.example.registrationOfCustomer.service;
 
 import com.example.registrationOfCustomer.model.DashboardModel;
 import com.example.registrationOfCustomer.model.RegistrationModel;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DashboardService {
 
     public String saveData(DashboardModel dashboardModel) {
--- src/main/java/com/example/registrationOfCustomer/service/RegistrationService.java
+++ src/main/java/com/example/registrationOfCustomer/service/RegistrationService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class RegistrationService {
-//this is sample
+
     @Autowired
     private RegistrationRepository registrationRepository;
 
