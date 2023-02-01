ada3babcc4689ef1470d26e2b2800b337c0330c9
Ramu Bista <114786134+tichkuleee@users.noreply.github.com>
2023-02-01 17:48:02 -0500

    Delete src/main directory

--- src/main/java/com/example/registrationOfCustomer/repository/DashboardRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.example.registrationOfCustomer.repository;
-//
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface DashboardRepository {
-//}
--- src/main/java/com/example/registrationOfCustomer/repository/LoginRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.registrationOfCustomer.repository;
-
-import com.example.registrationOfCustomer.entity.LoginEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoginRepository extends JpaRepository<LoginEntity, Integer> {
-
-
-}
